f854a2f20a063bb975acb3a1ab48a6e7a15446d9
numebegir <152449467+numebegir@users.noreply.github.com>
2023-12-05 17:51:52 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,26 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        }
+}
+        int[] data = new int[10];
+        for (int item : data) {
+
+public class RandomDataGenerator {
+    }
+        }
+def main():
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    for item in data:
+    main()
+
+
+import random
+    return data
+if __name__ == "__main__":
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
