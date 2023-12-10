953eab407402cd2fa156c960e4ee89372e045d53
numebegir <152449467+numebegir@users.noreply.github.com>
2023-12-10 23:29:45 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,14 @@
-    main()
-
     for item in data:
+def generate_random_data():
+def main():
+
+
+
+
+    main()
     data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-
         print(f"Random Number: {item}")
-def main():
-def generate_random_data():
-import random
-        print(f"Random Number: {item}")
-
-def main():
-    data = generate_random_data()
-    main()
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-def generate_random_data():
     return data
-
-if __name__ == "__main__":
